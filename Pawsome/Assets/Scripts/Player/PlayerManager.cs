d944ec57c5ade9c6fd63998e0d34d1ac8fcf7f78
Kevin <k.delcroix@rubika-edu.com>
2022-03-03 15:50:14 +0100

    Added BagSpell for lLayer

--- Pawsome/Assets/Scripts/Player/PlayerManager.cs
+++ Pawsome/Assets/Scripts/Player/PlayerManager.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class SpellBag
+{
+    public Spell MySpell;
+    public int InBag;
+    public SpellBag(Spell theSpell, int i)
+    {
+        this.MySpell = theSpell;
+        this.InBag = i;
+    }
+}
+
 public class PlayerManager : Singleton<PlayerManager>
 {
     public string PlayerName = "Nyan Cat";
     public double CatFood;
     public double CatBell;
-    public InstaCat[] MesChats;
+    public InstaCat[] MyCats;
+    public List<SpellBag> MyBagSpell = new List<SpellBag>();
 
     private void Awake()
     {
@@ -21,6 +33,38 @@ public class PlayerManager : Singleton<PlayerManager>
     public void IncrementCatBell(double x)
     {
         CatBell += x;
+    }
+    public void AddSpell(Spell TheSpell)
+    {
+        if(MyBagSpell.Count == 0)
+        {
+            MyBagSpell.Add(new SpellBag(TheSpell ,1));
+            return;
+        }
+        for(int i = 0; i > MyBagSpell.Count; i++)
+        {
+            if(MyBagSpell[i].MySpell == TheSpell)
+            {
+                MyBagSpell[i].InBag++;
+                return;
+            }
+        }
+    }
+    public bool UseSpell(Spell TheSpell)
+    {
+        if(MyBagSpell.Count == 0)
+        {
+            return false;
+        }
+        for (int i = 0; i > MyBagSpell.Count; i++)
+        {
+            if (MyBagSpell[i].MySpell == TheSpell && MyBagSpell[i].InBag > 0)
+            {
+                MyBagSpell[i].InBag--;
+                return true;
+            }
+        }
+        return false;
     }
     
 
